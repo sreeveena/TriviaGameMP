391880c9bd2299818622998864c898c3b9c021c4
sreeveena <sreeveena21@gmail.com>
2019-10-19 20:53:02 -0700

    created image objects in javascript.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,51 +5,61 @@ $(document).ready(function(){
             question: "In A Bug's Life, the bugs that Flik mistakes for warriors actually worked where?",
             answers: ['The theater', 'The circus', 'The mall', 'The farm'],
             correctAnswer: 'The circus',
+            image: "./images/bug'sLife.gif",
         },
         {
             question: "In Toy Story 2, what's the name of the 1950s puppet show in which Woody was the star?",
             answers: ['The Woody hour', 'Cowboy hour', 'Woody and the gang', 'Woody roundup'],
             correctAnswer: 'Woody roundup',
+            image: "./images/toy.gif",
         },
         {
             question: "When Monsters, Inc. opens up, who's the top scarer at the company?",
             answers: ['Mike', 'Sulley', 'Randall', 'Frank McCay'],
             correctAnswer: 'Sulley',
+            image: "./images/monsterInc.gif",
         },
         {
             question: "In The Incredibles, what's Frozone's actual first name?",
             answers: ['Lucas', 'Lewis', 'Lucius', 'Loomis'],
             correctAnswer: 'Lucius',
+            image: "./images/incredeables.gif",
         },
         {
             question: "In Cars, the Piston Cup is being held at what venue?",
             answers: ['Los Angeles International Speedway', 'Daytona International Speedway', 'Texas Motor Speedway', 'Talladega Superspeedway'],
             correctAnswer: 'Los Angeles International Speedway',
+            image: "./images/Piston_cup.png",
         },
         {
             question: "In WALL-E, what does EVE visit Earth in search of?",
             answers: ['Oil', 'Plant life', 'Friendship', 'Anything of value'],
             correctAnswer: 'Plant life',
+            image: "./images/wall-e.gif",
         },
         {
             question: "In Up, where is Paradise Falls located?",
             answers: ['Costa Rica', 'Chile', 'Guatemala', 'Venezuela'],
             correctAnswer: 'Venezuela',
+            image: "./images/up.gif",
         },
         {
             question: "Which character doesn't make an appearance in Toy Story 3?",
             answers: ['Slinky', 'Bo Peep', 'Rex', 'Mrs.Potato head'],
             correctAnswer: 'Bo Peep',
+            image: "./images/toyStory3.gif",
         },
         {
             question: "In Cars 2, where is the first race of the World Grand Prix held?",
             answers: ['London', 'Hong Kong', 'Tokyo', 'Paris'],
             correctAnswer: 'Tokyo',
+            image: "./images/cars2.gif",
         },
         {
             question: "What's the name of Monsters University's rival school in Monsters University?",
             answers: ['Fear University', 'Scare Tech', 'Scare State', 'Fear Tech'],
             correctAnswer: 'Fear Tech',
+            image: "./images/monsterUniversity.gif",
         },
 
 
@@ -71,7 +81,12 @@ var question;
 var option;
 //variable Done button to exit the trivia questions before the timer come to zero.
 var doneButton;
-
+// variable qi as question index
+var qi = 0;
+// variable ai as answer index
+var ai = 0;
+//variable to populate image object
+var img;
 //function timeRemaining will set the time in html timeRemaining class 
     //reducea the time every one second.
     //clears time if the timer reaches to 0 and calls the function timer.
@@ -93,18 +108,25 @@ var doneButton;
         timeRemain = 60;
         $(".timeRemaining").text("Time Remaining :" + timeRemain );
         // intervalId = setInterval(timeRemaining, 1000);
-        for(var i=0; i<myQuestions.length; i++){
-            question = $("<p style='font-size: 1.5em'>" + myQuestions[i].question + "</p>");
+            question = $("<p style='font-size: 1.5em'>" + myQuestions[qi].question + "</p>");
             $("#questions").append(question);
             // $("#questions").append($("<select class='answers'>"));
-            for( var j=0; j< myQuestions[i].answers.length; j++){
-                option = $("<div class='not-selected' value='"+myQuestions[i].answers[j]+"'>" + myQuestions[i].answers[j] + "</div>");
+            for( ai=0; ai< myQuestions[qi].answers.length; ai++){
+                option = $("<div class='not-selected' id='answer"+ai+"' value='"+myQuestions[qi].answers[ai]+"'>" + myQuestions[qi].answers[aj] + "</div>");
                 $("#questions").append(option);
+                $("#answer"+ ai).click(function(){
+                    // alert("answer is "+ $(this).attr("value"));
+                    // displayResult($(this).attr("value"));
+                     if($(this).attr("value") == myQuestions[qi].correctAnswer){
+                        img = 
+                     }
+
+                });
             }
             
         }
-    }
    
+  
 // function timer(){
 //     clearInterval(intervalId);
 //     results();
