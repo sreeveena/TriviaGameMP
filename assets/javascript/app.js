31dc07e6eae641a236281b60afca687e4aa8fb86
sreeveena <sreeveena21@gmail.com>
2019-10-19 22:15:50 -0700

    dynamically created html tags in startGame function,results and timeRemaining functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,61 +5,61 @@ $(document).ready(function(){
             question: "In A Bug's Life, the bugs that Flik mistakes for warriors actually worked where?",
             answers: ['The theater', 'The circus', 'The mall', 'The farm'],
             correctAnswer: 'The circus',
-            image: "./images/bug'sLife.gif",
+            image: "./assets/images/bugsLife.gif",
         },
         {
             question: "In Toy Story 2, what's the name of the 1950s puppet show in which Woody was the star?",
             answers: ['The Woody hour', 'Cowboy hour', 'Woody and the gang', 'Woody roundup'],
             correctAnswer: 'Woody roundup',
-            image: "./images/toy.gif",
+            image: "./assets/images/toy.gif",
         },
         {
             question: "When Monsters, Inc. opens up, who's the top scarer at the company?",
             answers: ['Mike', 'Sulley', 'Randall', 'Frank McCay'],
             correctAnswer: 'Sulley',
-            image: "./images/monsterInc.gif",
+            image: "./assets/images/monsterInc.gif",
         },
         {
             question: "In The Incredibles, what's Frozone's actual first name?",
             answers: ['Lucas', 'Lewis', 'Lucius', 'Loomis'],
             correctAnswer: 'Lucius',
-            image: "./images/incredeables.gif",
+            image: "./assets/images/incredeables.gif",
         },
         {
             question: "In Cars, the Piston Cup is being held at what venue?",
             answers: ['Los Angeles International Speedway', 'Daytona International Speedway', 'Texas Motor Speedway', 'Talladega Superspeedway'],
             correctAnswer: 'Los Angeles International Speedway',
-            image: "./images/Piston_cup.png",
+            image: "./assets/images/Piston_cup.png",
         },
         {
             question: "In WALL-E, what does EVE visit Earth in search of?",
             answers: ['Oil', 'Plant life', 'Friendship', 'Anything of value'],
             correctAnswer: 'Plant life',
-            image: "./images/wall-e.gif",
+            image: "./assets/images/wall-e.gif",
         },
         {
             question: "In Up, where is Paradise Falls located?",
             answers: ['Costa Rica', 'Chile', 'Guatemala', 'Venezuela'],
             correctAnswer: 'Venezuela',
-            image: "./images/up.gif",
+            image: "./assets/images/up.gif",
         },
         {
             question: "Which character doesn't make an appearance in Toy Story 3?",
             answers: ['Slinky', 'Bo Peep', 'Rex', 'Mrs.Potato head'],
             correctAnswer: 'Bo Peep',
-            image: "./images/toyStory3.gif",
+            image: "./assets/images/toyStory3.gif",
         },
         {
             question: "In Cars 2, where is the first race of the World Grand Prix held?",
             answers: ['London', 'Hong Kong', 'Tokyo', 'Paris'],
             correctAnswer: 'Tokyo',
-            image: "./images/cars2.gif",
+            image: "./assets/images/cars2.gif",
         },
         {
             question: "What's the name of Monsters University's rival school in Monsters University?",
             answers: ['Fear University', 'Scare Tech', 'Scare State', 'Fear Tech'],
             correctAnswer: 'Fear Tech',
-            image: "./images/monsterUniversity.gif",
+            image: "./assets/images/monsterUniversity.png",
         },
 
 
@@ -72,7 +72,7 @@ var inCorrectAnswers = 0;
 //variable unAnswered will have number of un answered questions a user has got in the trivia
 var unAnswered = 0;
 //variable to set time for the trivia
-var timeRemain = 120;
+var timeRemain = 20;
 //variable to set the time
 var intervalId;
 //variable question is to generate the question after start button is clicked
@@ -87,57 +87,96 @@ var qi = 0;
 var ai = 0;
 //variable to populate image object
 var img;
+var question1;
+var answer;
 //function timeRemaining will set the time in html timeRemaining class 
     //reducea the time every one second.
     //clears time if the timer reaches to 0 and calls the function timer.
-    // function timeRemaining(){
-    //     $(".timeRemaining").text("Time Remaining :" + timeRemain );
-    //     timeRemain--;
-    //     if(timeRemain == 0){
-    //         clearInterval(intervalId);
-    //         timer();
-    //     }
-    // }
+    function timeRemaining(){
+        $(".timeRemaining").text("Time Remaining :" + timeRemain );
+        timeRemain--;
+        if(timeRemain == 0){
+            clearInterval(intervalId);
+         $("#questions").remove();
+         intervalId4s = setInterval(nextQuestion, 4000);
+         answer = $("<div class='unAns'></div>");
+        $(".main").append(answer);
+        $(".unAns").append("<h4>Out of Time!</h4>"); 
+        $(".unAns").append("<h4>The correct answer was: "+myQuestions[qi].correctAnswer+"</h4>");
+        img = $("<img  style= 'width: 60px, height: 60px' src='"+myQuestions[qi].image+"'>");
+        $(".unAns").append(img);
+        unAnswered++;
+        }
+    }
 
 
     function startGame(){
         $(".start").remove();
-        correctAnswers = 0;
-        inCorrectAnswers = 0;
-        unAnswered = 0;
-        timeRemain = 60;
+        $(".unAns").remove();
+        $(".correctAns").remove();
+        $(".inCorrectAns").remove();
+        timeRemain = 20;
         $(".timeRemaining").text("Time Remaining :" + timeRemain );
-        // intervalId = setInterval(timeRemaining, 1000);
+         intervalId = setInterval(timeRemaining, 1000);
+         question1 = $("<div id='questions'></div>");
+            $(".main").append(question1);
             question = $("<p style='font-size: 1.5em'>" + myQuestions[qi].question + "</p>");
             $("#questions").append(question);
             // $("#questions").append($("<select class='answers'>"));
             for( ai=0; ai< myQuestions[qi].answers.length; ai++){
-                option = $("<div class='not-selected' id='answer"+ai+"' value='"+myQuestions[qi].answers[ai]+"'>" + myQuestions[qi].answers[aj] + "</div>");
+                option = $("<div class='not-selected' id='answer"+ai+"' value='"+myQuestions[qi].answers[ai]+"'>" + myQuestions[qi].answers[ai] + "</div>");
                 $("#questions").append(option);
                 $("#answer"+ ai).click(function(){
-                    // alert("answer is "+ $(this).attr("value"));
-                    // displayResult($(this).attr("value"));
-                     if($(this).attr("value") == myQuestions[qi].correctAnswer){
-                        img = 
-                     }
+                     results($(this).attr("value"));
 
                 });
             }
             
+    }
+    function nextQuestion(){
+        qi++;
+        clearInterval(intervalId4s);
+        if(qi< myQuestions.length){
+            startGame();
+        }else{
+            finalResult();
         }
-   
+       
+    }
+    function results(ans){
+        clearInterval(intervalId);
+
+        intervalId4s = setInterval(nextQuestion, 4000);
+        if(ans == myQuestions[qi].correctAnswer){
+            $("#questions").remove();
+            answer = $("<div class='correctAns'></div>");
+            $(".main").append(answer);
+            $(".correctAns").append("<h4>Correct !</h4>"); 
+            img = $("<img  style= 'width: 60px, height: 60px' src='"+myQuestions[qi].image+"'>");
+            $(".correctAns").append(img);
+            correctAnswers++;
+        } else{
+            $("#questions").remove();
+            answer = $("<div class='inCorrectAns'></div>");
+            $(".main").append(answer);
+            $(".inCorrectAns").append("<h4>Nope !</h4>"); 
+            $(".inCorrectAns").append("<h4>The correct answer was: "+myQuestions[qi].correctAnswer+"</h4>");
+            img = $("<img  style= 'width: 60px, height: 60px' src='"+myQuestions[qi].image+"'>");
+            $(".inCorrectAns").append(img);
+            inCorrectAnswers++;
+        }
+    }
   
-// function timer(){
-//     clearInterval(intervalId);
-//     results();
-//     $(".timeRemaining").remove();
+ function finalResult(){
+    clearInterval(intervalId);
+    $(".timeRemaining").remove();
 //     $("#questions").remove();
 //     $(".done").append("<h4 style= 'font-size:2em'> All Done!</h4>");
 //     $(".correctAns").append("<h4>Correct Answers :" + correctAnswers + "</h4>");
 //     $(".inCorrectAns").append("<h4> Incorrect Answers :" + inCorrectAnswers + "</h4>");
 //     $(".unAns").append("<h4>Unanswered :" + unAnswered + "</h4>"); 
 
-// }
+ }
  //startGame function is called when start button is clicked.
 $(".start").on("click", startGame); 
 })
